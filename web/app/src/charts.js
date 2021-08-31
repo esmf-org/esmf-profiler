4424e8111de5eed588061701849729ca6f72440d
Ryan Long <ryan@saltycatfish.com>
2021-08-30 20:45:12 -0400

    Default settings outside of class.

--- web/app/src/charts.js
+++ web/app/src/charts.js
@@ -1,36 +1,38 @@
 import Highcharts from 'highcharts';
 
+const defaultConfig = {
+    chart: {
+        type: "column", // column / bar
+    },
+    title: {
+        text: "Default Title", // graph title
+    },
+    xAxis: {
+        categories: ["ESM", "ABC"], // single bar label
+    },
+    yAxis: {
+        min: 0,
+        allowDecimals: false,
+        title: {
+            text: "Execution Times (in nanoseconds)",
+        },
+    },
+    legend: {
+        reversed: true,
+    },
+    plotOptions: {
+        series: {
+            stacking: "normal",
+        },
+    },
+};
+
 export default class StackedChart {
     constructor(container, chartObject) {
         this.containerNameString = container;
-        this.defaultConfig = {
-            chart: {
-                type: "column", // column / bar
-            },
-            title: {
-                text: "Default Title", // graph title
-            },
-            xAxis: {
-                categories: ["ESM", "ABC"], // single bar label
-            },
-            yAxis: {
-                min: 0,
-                allowDecimals: false,
-                title: {
-                    text: "Execution Times (in nanoseconds)",
-                },
-            },
-            legend: {
-                reversed: true,
-            },
-            plotOptions: {
-                series: {
-                    stacking: "normal",
-                },
-            },
-        };
+
         this.chartConfig = {
-            ...this.defaultConfig,
+            ...defaultConfig,
             ...chartObject
         }
     }
