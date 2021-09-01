24956e055db21d0dc37ae2ab5a482368d21053c0
Ryan Long <ryan@saltycatfish.com>
2021-08-31 22:57:54 -0400

    Added typescript.

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -1,39 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-
-    ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            // Prefer `dart-sass`
-                            implementation: require.resolve("sass"),
-                        },
-                    },
-                ],
+  mode: "development",
+  entry: {
+    index: "./src/index.ts",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require.resolve("sass"),
             },
+          },
         ],
-    }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
