1e717223fce43de008c506641635b8128365fcaf
Ryan Long <ryan@rlong.io>
2021-09-06 18:19:54 -0400

    Updated to context manager for profiling.  Couldn't get results with 'run'.

--- src/profiler/main.py
+++ src/profiler/main.py
@@ -1,3 +1,4 @@
+import cProfile
 from typing import List, cast
 from profiler.trace import RegionProfile, RegionProfiles, Trace
 import datetime
@@ -9,40 +10,40 @@ def stamp():
 
 def main():
 
-    include = ["region_profile"]
-    exclude = []
-    trace = cast(
-        List[RegionProfile],
-        list(
-            Trace.from_directory("./tests/fixtures/test-traces-large", include, exclude)
-        ),
-    )
+    with cProfile.Profile() as pr:
+        include = ["region_profile"]
+        exclude = []
+        trace = cast(
+            List[RegionProfile],
+            list(
+                Trace.from_directory(
+                    "./tests/fixtures/test-traces-large", include, exclude
+                )
+            ),
+        )
 
-    # Can filter in instructor
-    # trace = cast(
-    #     List[RegionProfile], list(trace.filter(lambda x: isinstance(x, RegionProfile)))
-    # )
-    # print(f"{stamp()}: creating tree")
-    # profiles = RegionProfiles(list(trace))._create_tree()
-    # # print(profiles.values())
-    # obj = dict(
-    #     zip(
-    #         profiles.keys(),
-    #         [profile.toJson().replace("\\", "") for profile in profiles.values()],
-    #     )
-    # )
-    # print(f"{stamp()}: complete")
-    # print(json.dumps(obj))
+        # Can filter in instructor
+        # trace = cast(
+        #     List[RegionProfile], list(trace.filter(lambda x: isinstance(x, RegionProfile)))
+        # )
+        # print(f"{stamp()}: creating tree")
+        # profiles = RegionProfiles(list(trace))._create_tree()
+        # # print(profiles.values())
+        # obj = dict(
+        #     zip(
+        #         profiles.keys(),
+        #         [profile.toJson().replace("\\", "") for profile in profiles.values()],
+        #     )
+        # )
+        # print(f"{stamp()}: complete")
+        # print(json.dumps(obj))
 
-    # summary = RegionSummary(trace)
-    # print(summary.pet_count())
-    # print(summary.count_each())
-    # for msg in trace:
+        # summary = RegionSummary(trace)
+        # print(summary.pet_count())
+        # print(summary.count_each())
 
-    #     print(msg)
+        pr.print_stats()
 
 
 if __name__ == "__main__":
-    import cProfile
-
-    cProfile.run("main()", "output.txt")
+    main()
