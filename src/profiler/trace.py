d34b31537e872034a1a84c17696e5e9e07d2f8da
Ryan Long <ryan@rlong.io>
2021-09-08 14:28:15 -0400

    Refactor trace for speed.

--- src/profiler/trace.py
+++ src/profiler/trace.py
@@ -1,9 +1,12 @@
-from abc import ABC, abstractproperty
-from typing import Any, Generator, List
-import bt2
-import textwrap
-from statistics import mean
+import bisect
 import json
+import textwrap
+from abc import ABC, abstractproperty
+from collections import namedtuple
+from statistics import mean
+from typing import Any, Dict, Generator, List
+
+import bt2
 
 
 class TraceEvent(ABC):
@@ -11,7 +14,7 @@ class TraceEvent(ABC):
         self._msg = msg
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} {textwrap.shorten(str(self.payload()), width=50)}>"
+        return f"<{self.__class__.__name__} {textwrap.shorten(str(self.payload), width=50)}>"
 
     def __str__(self):
         return self.toJson()
@@ -28,21 +31,33 @@ class TraceEvent(ABC):
     def get(self, key):
         try:
             if str(key).lower() in self.keys():
-                return self.payload()[str(key).lower()]
+                return self.payload[str(key).lower()]
         except KeyError:
             if key in dir(self):
-                return getattr(self, key)()
+                return getattr(self, key)
 
         raise AttributeError(f"Key '{key}' does not exist on {self.__class__.__name__}")
 
+    # @functools.cached_property
+    @property
     def payload(self):
         return self._msg.event.payload_field
 
+    # @functools.cached_property
+    @property
     def pet(self):
         return self._msg.event.packet.context_field["pet"]
 
     @staticmethod
     def Of(msg: bt2._EventMessageConst):
+        """Of Convnience constructor
+
+        Args:
+            msg (bt2._EventMessageConst):
+
+        Returns:
+            Obj:
+        """
         event_type = msg.event.name
         __map = {
             "define_region": lambda msg: DefineRegion(msg),
@@ -102,26 +117,20 @@ class RegionIdExit(TraceEvent):
 
 
 class RegionProfile(TraceEvent):
-    def __init__(self, msg):
-        super().__init__(msg)
-        self._children = []
-
     def keys(self):
         return [
             "id",
             "parentid",
             "total",
-            "self",
             "count",
             "max",
             "min",
             "mean",
             "pet",
-            "children",
         ]
 
-    def children(self):
-        return self._children
+
+Node = namedtuple("Node", ["petId", "id", "parentId"])
 
 
 class RegionProfiles:
@@ -137,47 +146,80 @@ class RegionProfiles:
     def toJson(self):
         return json.dumps(self, cls=RegionProfilesEncoder)
 
-    def _create_tree(self):
-        parentIds = self._filter_unique_values("parentId", [0])
-        petIds = self._filter_unique_values("pet")
-        petTree = self._build_pet_tree(petIds)
-        results = {}
-        for key, nodes in petTree.items():
-            results[str(key)] = self._build_tree(parentIds, nodes, "id", "parentId")
-        return results
+    def _create_tree(self, level: int = 1):
 
-    def _build_pet_tree(self, petIds):
-        results = {}
-        for petId in petIds:
-            results[petId] = list(
-                filter(lambda x: x.get("pet") == petId, self._profiles)
+        # for x in self._profiles:
+        #     print(f"{x.get('pet')},", f"{x.get('id')},", x.get("parentId"))
+        profiles = [
+            Node(
+                int(profile.get("pet")),
+                int(profile.get("id")),
+                int(profile.get("parentId")),
             )
+            for profile in self._profiles
+        ]
+
+        levels = self._determine_levels(profiles)
+        return list(filter(lambda x: x.get("id") in levels[1], self._profiles))
+
+    def _determine_levels(self, profiles: List[Node]):
+        parentIds = list(self._filter_unique_values("parentId", []))
+        levels = {}
+        for x in range(len(parentIds)):
+            parentId = parentIds[x]
+            levels[x] = list(
+                x.id for x in filter(lambda x: x.parentId == parentId, profiles)
+            )
+        return levels
+
+    def _get_level(self, profiles: List[Node], level: int = 1):
+        results = {}
+        parentIds = self._filter_unique_values("parentId", [])
+        print([x for x in parentIds])
+        results = filter(lambda x: x.id in parentIds, profiles)
+        for x in profiles:
+            if x.petId not in results.keys():
+                results[x.petId] = {}
+
+            if x.parentId not in results[x.petId].keys():
+                results[x.petId][x.parentId] = []
+            bisect.insort(results[x.petId][x.parentId], x.id)
+
         return results
 
-    def _build_tree(
-        self, rootIds, profiles: List[RegionProfile], parentIdKey, childIdKey
-    ):
-        results = []
-        for rootId in rootIds:
-            parentList = list(filter(lambda x: x.get(parentIdKey) == rootId, profiles))
-            parent = None
-            if len(parentList):
-                parent = parentList[0]
-                children = list(
-                    filter(
-                        lambda x: x.get(childIdKey) == rootId
-                        and isinstance(parent, TraceEvent)
-                        and x.get("pet") == parent.get("pet"),
-                        profiles,
-                    )
-                )
+    def _build_pet_tree(self, petIds, profiles: List[RegionProfile], accum: Dict = {}):
+        if len(petIds) > 0 and len(profiles) > 0:
+            petId = list(petIds)[0]
+            filtered = list(filter(lambda x: x.get("pet") == petId, profiles))
+            accum[petId] = filtered
+            remainder = list([x for x in set(profiles) if x not in set(filtered)])
+            return self._build_pet_tree(list(petIds)[1:], remainder, accum)
+        return accum
 
-                parent._children = children
-                yield parent
-            else:
-                print(f"Could not find parent for key {parentIdKey}")
-                continue
-        return RegionProfiles(results)
+    # def _build_tree(
+    #     self, rootIds, profiles: List[RegionProfile], parentIdKey, childIdKey
+    # ):
+    #     results = []
+    #     for rootId in rootIds:
+    #         parentList = list(filter(lambda x: x.get(parentIdKey) == rootId, profiles))
+    #         parent = None
+    #         if len(parentList):
+    #             parent = parentList[0]
+    #             children = list(
+    #                 filter(
+    #                     lambda x: x.get(childIdKey) == rootId
+    #                     and isinstance(parent, TraceEvent)
+    #                     and x.get("pet") == parent.get("pet"),
+    #                     profiles,
+    #                 )
+    #             )
+
+    #             parent._children = children
+    #             yield parent
+    #         else:
+    #             print(f"Could not find parent for key {parentIdKey}")
+    #             continue
+    #     return RegionProfiles(results)
 
     def _filter_unique_values(self, key: str, skip_values: List[Any] = []):
         return set(
@@ -257,7 +299,17 @@ class Trace:
     @staticmethod
     # TODO handle errors
     def from_directory(_path: str, include: List[str], exclude: List[str]):
+        hasInclude = len(include)
+        hasExclude = len(exclude)
+        results = []
         for msg in bt2.TraceCollectionMessageIterator(_path):
             if type(msg) is bt2._EventMessageConst:
-                if msg.event.name in include or msg.event.name not in exclude:
-                    yield TraceEvent.Of(msg)
+                event = msg.event.name
+
+                if (
+                    (hasInclude and event in include)
+                    or (hasExclude and event not in exclude)
+                    or (not hasInclude and hasExclude)
+                ):
+                    results.append(TraceEvent.Of(msg))
+        return results
