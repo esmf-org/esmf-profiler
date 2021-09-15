2c9ae6b75658c9c3249e2b0c805bf56c88f4d5ec
Rocky Dunlap <dunlap@ucar.edu>
2021-09-14 23:02:49 -0600

    Add new analyses class and subclasses and process the trace events immediately

---
+++ src/profiler/analyses.py
@@ -0,0 +1,124 @@
+from event import TraceEvent, DefineRegion, RegionProfile
+from abc import ABC, abstractproperty
+import logging
+
+logger = logging.getLogger(__name__)
+
+# an abstract class that all analyses will extend
+# examples include LoadBalance, MemoryProfile, etc.
+class Analysis(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractproperty
+    def process_event(self):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} requires a process_event method"
+        )
+
+    @abstractproperty
+    def finish(self):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} requires a finish method"
+        )
+
+
+class LoadBalance(Analysis):
+
+    # rootRegionName is the name of the region to be used as
+    # the root for the load balance plot
+    # if None, then use the top level
+    def __init__(self, rootRegionName):
+        self._rootRegionName = rootRegionName
+        # two level mapping
+        # { pet -> { region_id -> region name } }
+        self._regionIdToNameMap = {}
+
+        # map pet -> root of timing tree
+        self._timingTrees = {}
+
+    def process_event(self, event: TraceEvent):
+
+        if isinstance(event, DefineRegion):
+            pet = event.pet
+            regionid = event.get("id")
+            name = event.get("name")
+            #logger.debug(f"found DefineRegion pet = {pet}, id = {regionid}, name = {name}")
+            self._regionIdToNameMap.setdefault(pet, {})[regionid] = name
+
+
+        if isinstance(event, RegionProfile):
+            pet = event.pet
+            regionid = event.get("id")
+            total = event.get("total")
+            parentid = event.get("parentid")
+
+            if regionid == 1:
+                # special case for outermost timed region
+                name = "ROOT"
+            else:
+                map = self._regionIdToNameMap[pet]
+                name = map[regionid]  # should already be there
+
+            #logger.debug(f"found RegionProfile pet = {pet}, id = {regionid}, parentid = {parentid}, name = {name}, total time = {total}")
+
+            node = SinglePETTimingNode(id, name)
+            node.total = total
+
+            # add child to the timing tree for the given pet
+            root = self._timingTrees.setdefault(pet, SinglePETTimingNode(0,""))
+            root.add_child(parentid, node)
+
+    def finish(self):
+        # all events have been processed, so now we have a complete
+        # list of timing trees, one per PET in self._timingTrees
+
+        # use the information from the user to select which
+        # node will be the root node of focus for the load balance plot
+
+        if self._rootRegionName is None or self._rootRegionName == "":
+            # use the first level regions in the timing trees
+
+
+        else:
+            # find the root by searching the tree?
+            raise NotImplementedError(
+                f"{self.__class__.__name__} load balance only implemented for root level"
+            )
+
+    def toJSON(self):
+        # return JSON format for Javascript Charts
+        pass
+
+
+# class to represent as a tree the timing
+# information in the RegionProfile events
+# for now just deal with total times, since that
+# is all we need for the initial chart
+class SinglePETTimingNode():
+
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+        self._total = 0
+        self._children = []  # List[SinglePETTimingTreeNode]
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, value):
+        self._total = value
+
+    # add child node to the node with give parentid
+    def add_child(self, parentid, child: 'SinglePETTimingNode'):
+        if self._id == parentid:
+            self._children.append(child)
+            return True
+        else:
+            for c in self._children:
+                if c.add_child(parentid, child):
+                    return True
+        return False
--- src/profiler/main.py
+++ src/profiler/main.py
@@ -17,6 +17,8 @@ from profiler.utils import print_execution_time
 
 from profiler.trace import RegionProfile, RegionProfiles, Trace
 
+from analyses import Analysis, LoadBalance
+
 logger = logging.getLogger(__name__)
 _format = "%(asctime)s : %(levelname)s : %(name)s : %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=_format)
@@ -41,36 +43,45 @@ def main():
     """
 
     with cProfile.Profile() as pr:
+        #_path = "./tests/fixtures/test-traces/atm-ocn"
         _path = "./tests/fixtures/test-traces-large"
 
-        include = ["region_profile", "define_region"]
-        assert (
-            "define_region" in include
-        )  # TODO Until i have time to figure out something better
-        exclude = []
+        #include = ["region_profile", "define_region"]
+        #assert (
+        #    "define_region" in include
+        #)  # TODO Until i have time to figure out something better
+        #exclude = []
+
+        # the only requested analysis is a load balance at the root level
+        analyses = [LoadBalance(None)]
 
         logger.info("creating trace")
-        trace = Trace.from_path(_path, include, exclude)
+        trace = Trace.from_path(_path, analyses)
         logger.debug("complete")
 
-        logger.info("creating lookup table for region profiles")
-        lookup = Lookup(trace)
-        logger.debug("complete")
+        # indicate to the analyses that all events have been processed
+        for analysis in analyses:
+            analysis.finish()
+            # analysis.toJSON()
 
-        logger.info("creating lookup table for region profiles")
-        region_profiles = list(filter(lambda x: isinstance(x, RegionProfile), trace))
-        logger.debug("complete")
+        #logger.info("creating lookup table for region profiles")
+        #lookup = Lookup(trace)
+        #logger.debug("complete")
 
-        logger.info("Looking up and appending names to Region Profiles")
-        for item in region_profiles:
-            attribs = {"name": lookup.find(item.get("pet"), item.get("id"))}
-            item._attributes = attribs
-        logger.debug("complete")
+        #logger.info("creating lookup table for region profiles")
+        #region_profiles = list(filter(lambda x: isinstance(x, RegionProfile), trace))
+        #logger.debug("complete")
 
-        logger.info(f"creating tree from list of region profiles")
-        tree = RegionProfiles(iter(region_profiles), lookup)._create_tree_json()
-        logger.debug("complete")
-        print(tree)
+        #logger.info("Looking up and appending names to Region Profiles")
+        #for item in region_profiles:
+        #    attribs = {"name": lookup.find(item.get("pet"), item.get("id"))}
+        #    item._attributes = attribs
+        #logger.debug("complete")
+
+        #logger.info(f"creating tree from list of region profiles")
+        #tree = RegionProfiles(iter(region_profiles), lookup)._create_tree_json()
+        #logger.debug("complete")
+        #print(tree)
 
         # Uncomment for Profiling
         # s = io.StringIO()
