1df45b5185148cdb339341982f9f80abd47f20e3
Ryan Long <ryan.long@noaa.gov>
2021-09-23 10:52:22 -0600

    Change install_swig to install

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 SWIG='swig-4.0.2'
 PCRE='pcre-8.37'
 BT2='babeltrace2-2.0.4'
@@ -13,18 +14,20 @@ tar -xovzf ${SWIG}.tar.gz || exit 1
 cd $SWIG
 # We first have to install PCRE.
 echo "Installing SWIG dependency ${PCRE}"
-if [ ! -e ${PCRE}.tar.gz ]; then
-  wget -T 10 -t 3 \
-    https://sourceforge.net/projects/pcre/files/pcre/8.37/${PCRE}.tar.gz || exit 1;
+# if [ ! -e ${PCRE}.tar.gz ]; then
+#   wget -T 10 -t 3 \
+#     https://sourceforge.net/projects/pcre/files/pcre/8.37/${PCRE}.tar.gz || exit 1;
     
-fi
-Tools/pcre-build.sh
+# fi
+# Tools/pcre-build.sh
 ./configure --prefix=/glade/u/home/rlong/opt/swig
 make
 make install
 cd ..
 
 export PATH=/glade/u/home/rlong/opt/swig/bin:$PATH
+export PYTHONPATH=/glade/u/home/rlong/opt/bt2/lib/python3.7/site-packages:$PYTHONPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/glade/u/home/rlong/opt/bt2/lib
 
 echo "Installing ${BT2}"
 if [ ! -e ${BT2}.tar.bz2 ]; then
@@ -34,10 +37,10 @@ fi
 tar -xojvf ${BT2}.tar.bz2 || exit 1
 cd ${BT2}
 pip install python-config
-./configure --host=x86_64 --build=x86_64 --target=x86_64 --prefix=/glade/u/home/rlong/opt/bt2 --enable-python-bindings --enable-python-plugins --disable-man-pages --disable-debug-info --disable-shared
+./configure --prefix=/glade/u/home/rlong/opt/bt2 --enable-shared --enable-python-bindings --enable-python-plugins --disable-man-pages --disable-debug-info
 make
 make install
 cd ..
 
-export PYTHONPATH=$PYTHONPATH:/glade/u/home/rlong/opt/bt2/lib/python3.7/site-packages
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/glade/u/home/rlong/opt/bt2/lib
+
+
