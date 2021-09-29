455af56ea120356c636a19dd7328b1c6464ed98e
Ryan Long <ryan@saltycatfish.com>
2021-09-29 11:15:00 -0400

    Remove copyright from footer

--- web/app/src/components/Footer.jsx
+++ web/app/src/components/Footer.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-
+import React from "react";
 
 export default class Footer extends React.Component {
-
-
-    render() {
-        return (
-            <footer className="sticky-footer bg-white">
-                <div className="container my-auto">
-                    <div className="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-
-        )
-    }
+  render() {
+    return (
+      <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span></span>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
