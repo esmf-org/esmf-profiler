c319458b91a2c92683f3d8f2754241e18de8ee94
Ryan Long <ryan@saltycatfish.com>
2021-09-30 01:25:57 -0400

    Create button to hide sidebar

--- web/app/src/components/SideBar.jsx
+++ web/app/src/components/SideBar.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import { useState } from "react";
+
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import Button from "react-bootstrap/Button";
+
+import Modal from "react-bootstrap/Modal";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHubspot } from "@fortawesome/free-brands-svg-icons";
 import {
@@ -9,100 +20,119 @@ import {
   faHdd,
   faArrowCircleLeft,
 } from "@fortawesome/free-solid-svg-icons";
+import styled, { ThemeProvider } from "styled-components";
 
-export default class Sidebar extends React.Component {
-  render() {
-    return (
-      <ul
-        className="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion"
-        id="accordionSidebar"
-      >
-        {/* Sidebar - Brand */}
-        <a
-          className="sidebar-brand d-flex align-items-center justify-content-center"
-          href="index.html"
-        >
-          <div className="sidebar-brand-icon rotate-n-15">
-            <i className="fas fa-laugh-wink"></i>
-          </div>
-          <div className="sidebar-brand-text mx-3">
-            ESMF Profiler{" "}
-            <div>
-              {/* TODO Add JS to dynamically update version based on tag */}
-              <img src="https://img.shields.io/badge/version-0.1.0-success"></img>
+const open = styled.div`
+  text-decoration: none;
+  font-size: 1.1rem;
+  padding: 1.5rem 1rem;
+  text-align: center;
+  letter-spacing: 0.05rem;
+`;
+
+const closed = styled.div`
+  width: 10px;
+`;
+
+export default function Sidebar() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <React.Fragment>
+      <br />
+
+      <div style={show ? { width: "10px" } : { width: "auto" }}>
+        <Nav className="flex-column" bg="gradient-dark">
+          <ul className="navbar-nav bg-gradient-dark sidebar sidebar-dark">
+            {/* Sidebar - Brand */}
+            <div className="sidebar-brand d-flex align-items-center justify-content-center">
+              <div className="sidebar-brand-text">
+                ESMF Profiler{" "}
+                <div>
+                  {/* TODO Add JS to dynamically update version based on tag */}
+                  <img src="https://img.shields.io/badge/version-0.1.0-success"></img>
+                </div>
+              </div>
             </div>
-          </div>
-        </a>
 
-        {/* Divider */}
-        <hr className="sidebar-divider my-10" />
-        <div className="sidebar-heading">Application Info</div>
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faBox} />
-            <span> Component Configuration</span>
-          </a>
-        </li>
+            {/* Divider */}
+            <hr className="sidebar-divider my-10" />
+            <div className="sidebar-heading">Application Info</div>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faBox} />
+                <span> Component Configuration</span>
+              </Link>
+            </li>
 
-        {/* Divider */}
-        <hr className="sidebar-divider" />
+            {/* Divider */}
+            <hr className="sidebar-divider" />
 
-        {/* Heading */}
-        <div className="sidebar-heading">Timing</div>
+            {/* Heading */}
+            <div className="sidebar-heading">Timing</div>
 
-        {/* Nav Item - Pages Collapse Menu */}
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faClock} />
-            <span> Timing Summary</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faChartBar} />
-            <span> Load Balance</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faProjectDiagram} />
-            <span> MPI Profile</span>
-          </a>
-        </li>
+            {/* Nav Item - Pages Collapse Menu */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faClock} />
+                <span> Timing Summary</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faChartBar} />
+                <span> Load Balance</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faProjectDiagram} />
+                <span> MPI Profile</span>
+              </Link>
+            </li>
 
-        {/* Divider */}
-        <hr className="sidebar-divider" />
+            {/* Divider */}
+            <hr className="sidebar-divider" />
 
-        {/* Heading */}
-        <div className="sidebar-heading"> Memory</div>
+            {/* Heading */}
+            <div className="sidebar-heading"> Memory</div>
 
-        {/* Nav Item - Pages Collapse Menu */}
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faHubspot} />
-            <span> Memory Profile</span>
-          </a>
-        </li>
+            {/* Nav Item - Pages Collapse Menu */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faHubspot} />
+                <span> Memory Profile</span>
+              </Link>
+            </li>
 
-        {/* Nav Item - Charts */}
-        <div className="sidebar-heading">I/O</div>
-        <li className="nav-item">
-          <a className="nav-link" href="charts.html">
-            <FontAwesomeIcon icon={faHdd} />
-            <span> NetCDF Profile</span>
-          </a>
-        </li>
+            {/* Nav Item - Charts */}
+            <div className="sidebar-heading">I/O</div>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faHdd} />
+                <span> NetCDF Profile</span>
+              </Link>
+            </li>
 
-        {/* Divider */}
-        <hr className="sidebar-divider d-none d-md-block" />
+            {/* Divider */}
+            <hr className="sidebar-divider d-none d-md-block" />
 
-        {/* Sidebar Toggler (Sidebar) */}
-        <div className="text-center d-none d-md-inline">
-          <button className="rounded-circle border-0" id="sidebarToggler">
+            {/* Sidebar Toggler (Sidebar) */}
+
+            {/* <button className="rounded-circle border-0" id="sidebarToggler">
             <FontAwesomeIcon icon={faArrowCircleLeft} />
-          </button>
-        </div>
-      </ul>
-    );
-  }
+          </button> */}
+            <Button
+              onClick={() => {
+                setShow(() => !show);
+                console.log(show);
+              }}
+            >
+              Open Menu
+            </Button>
+          </ul>
+        </Nav>
+      </div>
+    </React.Fragment>
+  );
 }
