41f40dd12c18db295b9e440acbb15cd884234b21
Ryan Long <ryan@saltycatfish.com>
2021-10-11 16:39:22 -0400

    Add site name and timestamp as headers in Report.

--- web/app/src/components/Report.jsx
+++ web/app/src/components/Report.jsx
@@ -9,53 +9,58 @@ import AlertDismissible from "./alerts/AlertDismissible";
 
 function Report() {
   const [data, setData] = useState();
-  const [title, setTitle] = useState();
+  const [title, setTitle] = useState({
+    title: "",
+    timestamp: "",
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
+    // Fetch Function
+    fetch("data/load_balance.json")
+      .then(function (res) {
+        //console.log("lb response")
+        return res.json();
+      })
+      .then(function (_data) {
+        //console.log("lb setData")
+        setData(_data);
+      })
+      .catch(function (err) {
+        console.log(err, "Error loading data/load_balance.json");
+      });
 
-      // Fetch Function
-      fetch("data/load_balance.json")
-        .then(function (res) {
-          //console.log("lb response")
-          return res.json();
-        })
-        .then(function (_data) {
-          //console.log("lb setData")
-          setData(_data);
-        })
-        .catch(function (err) {
-          console.log(err, "Error loading data/load_balance.json");
+    fetch("data/site.json")
+      .then(function (res) {
+        //console.log("site response = " + res)
+        return res.json();
+      })
+      .then(function (_site) {
+        //console.log("setting title")
+        console.log(_site);
+        setTitle({
+          title: _site.name + " - ESMF Profiler",
+          timestamp: _site.timestamp,
         });
-
-       fetch("data/site.json")
-         .then(function (res) {
-          //console.log("site response = " + res)
-          return res.json();
-        })
-        .then(function (_site) {
-          //console.log("setting title")
-          setTitle(_site.name + " - ESMF Profiler");
-        })
-        .catch(function (err) {
-          console.log(err, "Error loading data/site.json");
-        });
-
-    },[])
+      })
+      .catch(function (err) {
+        console.log(err, "Error loading data/site.json");
+      });
+  }, []);
 
   return (
     <div className="App">
-      <Helmet title={title}></Helmet>
+      <Helmet title={`${title.title} - ${title.timestamp}`}></Helmet>
       <div id="wrapper">
         <Sidebar />
         <div id="content-wrapper" className="d-flex flex-column">
           <div id="content">
             <div className="container-fluid bg-white">
               <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800"></h1>
+                <h1 className="h3 mb-0 text-gray-800">{title.title}</h1>
+                <sup>{title.timestamp}</sup>
               </div>
 
               <div className="row">
-                
                 {data ? (
                   <ChartContainer>
                     <Stacked options={data} />
