d858725693d3b027d47b1a58b660f91eb740a0e9
Rocky Dunlap <dunlap@ucar.edu>
2021-10-16 21:42:37 -0600

    Working on select all/none

--- web/app/src/charts/Stacked.jsx
+++ web/app/src/charts/Stacked.jsx
@@ -3,14 +3,16 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import HC_exporting from "highcharts/modules/exporting";
 
-import { useState, useEffect } from "react";
+//import Boost from "highcharts/modules/boost";
+
+import { useState, useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import AlertDismissible from "./../components/alerts/AlertDismissible";
 
 HC_exporting(Highcharts);
-
+//Boost(Highcharts);
 
 function Stacked(props) {
   const chartOptions = {
@@ -33,7 +35,7 @@ function Stacked(props) {
       min: 0,
       allowDecimals: true,
       title: {
-        text: "Time (s)",
+        text: "Total Time (s)",
       },
     },
     legend: {
@@ -45,6 +47,7 @@ function Stacked(props) {
     plotOptions: {
       series: {
         stacking: "normal",
+        //boostThreshold: 1,
         events: {
           click: function () {
             clickLevel(this.name);
@@ -62,58 +65,57 @@ function Stacked(props) {
 
   const [error, setError] = useState("");
   const [level, setLevel] = useState(["/TOP"]);
+  const prevLevel = useRef(["/TOP"])
   const [options, setOptions] = useState(chartOptions);
+  const chartComponent = useRef(null);
 
   useEffect(() => {
+    prevLevel.current = level;
     updateLevel();
   }, [level]);
 
   const updateLevel = () => {
-    //TODO:  add a way to dismiss the error message since
-    //       resetting it here clears the error before the user sees it
-    //       Consider switching to a popup.
-    //setError("");
+
+    setError("");
 
     const key = level.join("/");
 
     if (!props.options[key]) {
-      setError("no data available");
-      // setLevel((prev) => [...prev.slice(0, prev.length - 1)]);
+      setError(`No detail timing data for ${key}`);
       return;
     }
 
     let _new = {
       xAxis: {
-        categories: JSON.parse(JSON.stringify(props.options[key].xvals))
+        categories: [...(props.options[key].xvals)]
       },
-      series: JSON.parse(JSON.stringify(props.options[key].yvals))
+      series:
+        props.options[key].yvals.map(
+            (y) => { return {name: y.name, visible: true, data: [...y.data]};}),
     };
+
+    //console.debug("...yvals = ", [...(props.options[key].yvals)])
+    //console.debug("_new = ", _new)
+
     setOptions((prevOptions) => {
       return { ...prevOptions, ..._new };
     });
   };
 
-  const toggleOn = () => toggleAllSeries(true);
-  const toggleOff = () => toggleAllSeries(false);
-
-  const toggleAllSeries = (value) => {
-    console.debug(`toggleAllSeries(${value})`);
-    setOptions((prev) => {
-      return {
-        ...prev,
-        plotOptions: {
-          series: {
-            visible: value,
-          },
-        },
-      };
-    });
-  };
+  const toggle = (show) => {
+    const chart = chartComponent.current.chart;
+    if (show) {
+        chart.series.map((s) => s.setVisible(true, false));
+    }
+    else {
+        chart.series.map((s) => s.setVisible(false, false));
+    }
+    chart.redraw()
+  }
 
 
   const hasData = (check) => {
     const key = check.join("/")
-    console.debug("hasData: key = ", key)
     if (!props.options[key]) {
       return false;
     }
@@ -121,42 +123,24 @@ function Stacked(props) {
   };
 
   const clickLevel = (_level) => {
-    //if (!hasData(_level)) {
-    //  setError(`No data available for ${_level}`);
-    //  return;
-    //}
     console.debug(`clickLevel(${_level})`);
-    let err = "";
-    setLevel((current) => {
-        //NOTE:  I had to move this in here to access
-        //       the previous value correctly.
-        if (hasData([...current, _level])) {
-            return [...current, _level];
-        }
-        else {
-            err = "No deeper timing information available";
-            return [...current];
-        }
-    });
 
-    if (err != "") {
-        setError(err);
+    if (!hasData([...prevLevel.current, _level])) {
+      setError(`No detail timing data for ${_level}`);
     }
-
+    else {
+        setLevel((current) => [...current, _level]);
+    }
   };
 
   const clickCrumb = (idx, _level) => {
-    //if (!hasData(_level)) {
-    //  setError(`No data available for ${_level}`);
-    //  return;
-    //}
     console.debug(`clickCrumb(${idx}, ${_level})`);
     setLevel((prev) => prev.slice(0, idx + 1));
   };
 
   return (
     <React.Fragment>
-      {error && <AlertDismissible message={error} />}
+      {error && <AlertDismissible message={error}/>}
 
       <Breadcrumb>
         {level?.map
@@ -174,14 +158,14 @@ function Stacked(props) {
           : ""}
       </Breadcrumb>
 
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <HighchartsReact highcharts={Highcharts} options={options} ref={chartComponent}/>
 
       <div className="d-flex justify-content-center">
         <ButtonGroup size="sm" className="me-2">
-          <Button onClick={toggleOn} className="m-1">
+          <Button onClick={() => toggle(true)} className="m-1">
             Select All
           </Button>
-          <Button onClick={toggleOff} className="m-1">
+          <Button onClick={() => toggle(false)} className="m-1">
             Select None
           </Button>
         </ButtonGroup>
