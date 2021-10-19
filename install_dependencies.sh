b0d45e2c37d3d31de9155851e7918f242afeae07
Ryan Long <ryanlong1004@gmail.com>
2021-10-19 19:19:39 -0400

    Add version compare function that accounts for different version lengths

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
 
 gccver="$(gcc -dumpversion)"
 requiredgcc="9.0.0"
-if [ "$(printf '%s\n' "$requiredgcc" "$gccver" | sort -V | head -n1)" = "$requiredgcc" ]; then 
+
+vercomp $gccver $requiredgcc
+if [[ $? < 1 ]]; then 
     echo "Found gcc version ${gccver}"
 else
     echo "Error:  The Babeltrace2 dependent library require gcc version of at least ${requiredgcc}.  Make sure gcc 9+ is in your path before running this script."
