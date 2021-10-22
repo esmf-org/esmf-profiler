acfee4995b11f36cba8ea2de345872c288c9dc79
Ryan Long <ryanlong1004@gmail.com>
2021-10-22 14:05:39 -0400

    Add name arg to pass to report name.

--- go.sh
+++ go.sh
@@ -1,3 +1,8 @@
-docker build .
+#!/bin/bash
 
-docker run -it -v $(pwd)/traces:/home/traces test9 esmf-profiler -t /home/traces -n 'test' -o /home/traces/output
+DEFAULT_OUTPUT_NAME="default"
+OUTPUT_NAME="${1:-$DEFAULT_OUTPUT_NAME}"
+
+docker build -t test9 .
+
+docker run -it -v $(pwd)/traces:/home/traces test9 esmf-profiler -t /home/traces -n $1 -o /home/traces/output
