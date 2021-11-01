6752c8882e6f898e2f9e611e1bf81edb0fb29563
Ryan Long <ryanlong1004@gmail.com>
2021-11-01 14:10:09 -0400

    Add run_analysis test

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,20 +1,36 @@
+import os
 import tempfile
 
-from profiler.main import create_site_file
-import os
+from profiler.main import create_site_file, run_analsysis
 
 
-def test_injectReportTemplate_injectsReportTemplate():
+def test_createSiteFile_createsSiteFileCorrectly():
     with tempfile.TemporaryDirectory() as _temp:
         create_site_file("test", _temp, "site.json")
 
         file_path = os.path.join(_temp, "site.json")
         assert os.path.exists(file_path)
 
-        contents = open(file_path, "r").read()
-        assert "name" in contents
-        assert "test" in contents
-        assert "timestamp" in contents
+        with open(file_path, "r") as _file:
+            contents = _file.read()
+            assert "name" in contents
+            assert "test" in contents
+            assert "timestamp" in contents
+
+
+def test_runAnalysis_givesTheCorrectOutput():
+    with tempfile.TemporaryDirectory() as _temp:
+        run_analsysis(_temp, "tests/fixtures/test-traces", "load_balance.json")
+
+        file_path = os.path.join(_temp, "load_balance.json")
+        assert os.path.exists(file_path)
+
+        with open(file_path, "r") as _file:
+            with open("tests/fixtures/test-traces/expected.json") as _expected:
+                contents = _file.read()
+                expected = _expected.read()
+
+                assert contents == expected
 
 
 if __name__ == "__main__":
