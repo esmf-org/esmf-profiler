3056198f13fbcadb09b16780d8281b9ff40ed673
Ryan Long <ryan@saltycatfish.com>
2021-11-03 18:38:38 -0400

    Update version in setup.py and add tag script to be added as hook

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",  # Required
+    version="0.1",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -126,8 +126,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        #    "python-config==0.1.2",  # TODO not 100% if we need this
-        #    "python-dev-tools==2020.9.10",
+        
     ],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
@@ -138,7 +137,15 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={  # Optional
-        "dev": ["check-manifest", "black"],
+        "dev": [
+            "check-manifest",
+            "black",
+            "pytest",
+            "pytest-cov",
+            "rope",
+            "flake8",
+            "pip-tools",
+        ],
         "test": ["pytest", "pytest-cov"],
     },
     # If there are data files included in your packages that need to be
