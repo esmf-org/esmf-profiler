dda1fca64f2108b86ad922f9e703cd06532ed7e5
Ryan Long <ryanlong1004@gmail.com>
2021-11-04 16:06:47 -0400

    Add tag script to keep setup.py and git tags in sync.

--- tag.sh
+++ tag.sh
@@ -1,1 +1,13 @@
-git tag -a v$(python setup.py --version) -m 'description of version'
+#!/bin/bash
+
+# tag.sh
+# Ryan Long <ryan.long@noaa.gov>
+
+# Used to keep setup.py and Git versions in sync.  
+
+# Reads the version from setup.py and attempts to create
+# a git tag based off of it.
+
+echo "Enter a version description."
+read VERSION_DESCRIPTION
+git tag -a v$(python setup.py --version) -m $VERSION_DESCRIPTION
