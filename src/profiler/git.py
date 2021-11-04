ef6a5f36871a0523ccc7bfc4b4423015417055a6
Ryan Long <ryanlong1004@gmail.com>
2021-11-04 15:57:15 -0400

    Document and log.  Make Git Add generic like the rest of the commands.

--- src/profiler/git.py
+++ src/profiler/git.py
@@ -1,11 +1,17 @@
 """
 Git commands module
+
+Subprocess convenience module for interacting with Git
+
 author: Ryan Long <ryan.long@noaa.gov>
 """
 
 import glob
 import os
 import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def _command_safe(cmd, cwd=os.getcwd()):
@@ -13,33 +19,82 @@ def _command_safe(cmd, cwd=os.getcwd()):
     on error
     """
     try:
+        logger.debug("running '%s' in '%s'", cmd, cwd)
         return subprocess.run(
             cmd, cwd=cwd, check=True, stdout=subprocess.PIPE, encoding="utf-8"
         )
-    except Exception as e:
-        print(f"Subprocess error: {e}")
+    except Exception as error:
+        logger.error("Subprocess error: %s", error)
+        raise
 
 
-def git_add(profilepath, repopath=os.getcwd()):
-    cmd = ["git", "add"] + glob.glob(profilepath + "/*")
+def git_add(_path, repopath=os.getcwd()):
+    """git_add
+
+    Args:
+        _path (str): path of assets to add
+        repopath (str, optional): local repository path if not cwd. Defaults to os.getcwd().
+
+    Returns:
+        CompletedProcess:
+    """
+    cmd = ["git", "add"] + glob.glob(_path + "/*")
     return _command_safe(cmd, repopath)
 
 
-def git_commit(username, name, repopath=os.getcwd()):
-    cmd = ["git", "commit", "-a", "-m", f"'Commit profile {username}/{name}'"]
+def git_commit(message, repopath=os.getcwd()):
+    """git_commit
+
+    Args:
+        username (str):
+        name (str): name of report to commit
+        repopath (str, optional): local repository path if not cwd. Defaults to os.getcwd().
+
+    Returns:
+        CompletedProcess:
+    """
+    cmd = ["git", "commit", "-a", "-m", f"'{message}'"]
     return _command_safe(cmd, repopath)
 
 
-def git_pull(repopath=os.getcwd(), destination="origin"):
+def git_pull(destination="origin", repopath=os.getcwd()):
+    """git_pull
+
+    Args:
+        destination (str, optional): Defaults to "origin".
+        repopath (str, optional): Defaults to os.getcwd().
+
+    Returns:
+        CompletedProcess
+    """
+
     cmd = ["git", "pull", destination]
     return _command_safe(cmd, repopath)
 
 
-def git_push(repopath=os.getcwd(), destination="origin"):
+def git_push(destination="origin", repopath=os.getcwd()):
+    """git_push
+
+    Args:
+        destination (str, optional): Defaults to "origin".
+        repopath (str, optional): Defaults to os.getcwd().
+
+    Returns:
+        CompletedProcess
+    """
     cmd = ["git", "push", destination]
     return _command_safe(cmd, repopath)
 
 
-def git_clone(url, tmpdir):
-    cmd = ["git", "clone", url, tmpdir]
-    return _command_safe(cmd, tmpdir)
+def git_clone(url, target_path):
+    """git_clone
+
+    Args:
+        url (str): remote url
+        target_path (str): local target path
+
+    Returns:
+        CompletedProcess
+    """
+    cmd = ["git", "clone", url, target_path]
+    return _command_safe(cmd, target_path)
