a50bf804e2d62f4ab3a43e9247d23cb450231522
Ryan Long <ryanlong1004@gmail.com>
2021-11-09 17:39:58 -0500

    Revert to install.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -55,8 +55,10 @@ cd $SWIG
 ./configure --prefix=$PWD/../INSTALL/$SWIG --without-go --without-r --without-lua --without-csharp --without-ocaml --without-ruby --without-mzscheme --without-java --without-javascript --without-octave --without-scilab
 make clean
 make -j6
-./preinst-swig -python -copyright || exit 1
+make install
 cd ..
+
+export PATH=$PWD/INSTALL/$SWIG/bin:$PATH
 
 echo "Installing ${BT2}"
 if [ ! -e ${BT2}.tar.bz2 ]; then
@@ -65,7 +67,7 @@ fi
 tar -xojvf ${BT2}.tar.bz2 || exit 1
 cd $BT2
 
-./configure --prefix=$PWD/../INSTALL/$BT2 --enable-shared --enable-python-bindings --enable-python-plugins --disable-man-pages --disable-debug-info
+./configure --prefix=$PWD/../INSTALL/$BT2 --enable-static --enable-python-bindings --enable-python-plugins --disable-man-pages --disable-debug-info
 make clean
 make -j6
 make install
