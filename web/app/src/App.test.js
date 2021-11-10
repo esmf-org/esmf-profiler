fffd094199e25bbed7189eb390e39988ecad8650
Ryan Long <ryan@saltycatfish.com>
2021-11-09 20:15:46 -0500

    Add the rest of build

--- web/app/src/App.test.js
+++ web/app/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Report from "./components/Card"
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Card />);
+  const linkElement = screen.getByText(/esmf/i);
   expect(linkElement).toBeInTheDocument();
 });
