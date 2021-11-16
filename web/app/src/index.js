e65f976e9ce0e505de701bac011d02f335e8cacf
Ryan Long <ryan@saltycatfish.com>
2021-11-16 18:24:33 -0500

    Move app out of index and remove Router

--- web/app/src/index.js
+++ web/app/src/index.js
@@ -1,37 +1,10 @@
 import './styles/main.scss'
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Report from "./components/Report"
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import App from "./components/App"
 
-
-function App() {
-  return (
-    <div>
-      <HelmetProvider>
-        <Helmet>
-          <title>ESMF Profiler</title>
-          <link rel="canonical" href="#" />
-        </Helmet>
-
-        <Switch>
-          <Route path="/">
-            <Report />
-          </Route>
-        </Switch>
-      </HelmetProvider>
-    </div>
-  );
-}
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <App />,
   document.getElementById("root")
 );
