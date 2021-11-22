1318c956d8a21a3789c7a34bbf0138a1f11e222b
Ryan Long <ryan@saltycatfish.com>
2021-11-22 13:25:52 -0500

    Initial work on styling and timed-closing

--- web/app/src/components/alerts/AlertDismissible.jsx
+++ web/app/src/components/alerts/AlertDismissible.jsx
@@ -1,16 +1,37 @@
-import { Alert, Row, Col } from "react-bootstrap";
-import styled from "styled-components";
+import { Alert, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-const DismissButton = styled.div`
-  padding: 0px;
-`;
+// import styled from "styled-components";
+
+// const DismissButton = styled.div`
+//   padding: 0px;
+// `;
 
 function AlertDismissible(props) {
+  const [show, setShow] = useState(true);
+
   return (
-    <Alert variant="danger">
-      <Row>
-        <Col>{props.message}</Col>
-      </Row>
+    // <Alert variant="danger">
+    //   <div className="d-flex justify-content-end">
+    //     <button
+    //       type="button"
+    //       class="close"
+    //       data-dismiss="alert"
+    //       aria-hidden="true"
+    //     >
+    //       x
+    //     </button>
+    //   </div>
+    //   <Alert.Heading>Error</Alert.Heading>
+    //   <p>{props.message}</p>
+    // </Alert>
+    <Alert show={show} variant={props.variant ? props.variant : "danger"}>
+      <div className="d-flex justify-content-end">
+        <Button onClick={() => setShow(false)} variant="outline-danger">
+          X
+        </Button>
+      </div>
+      {props.message}
     </Alert>
   );
 }
