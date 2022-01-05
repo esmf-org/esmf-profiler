44e13f37cea31f6807093b5830f02e7eb7e4c245
Ryan Long <ryan.long@noaa.gov>
2022-01-05 01:11:17 +0000

    Fix script to use parent dirs

--- scripts/q.sh
+++ scripts/q.sh
@@ -1,1 +1,1 @@
-esmf-profiler -t ./tests/fixtures/test-traces -n "test20211122" -o "test20211122" -p 'https://github.com/ryanlong1004/automatic-succotash'
+esmf-profiler -t ./tests/fixtures/test-traces/atm-ocn -n "test20211122" -o "test20211122" -p 'https://github.com/ryanlong1004/automatic-succotash'
