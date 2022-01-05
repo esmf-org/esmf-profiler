07f15135cbcd11e69d226477abb3b662d98be329
Ryan Long <ryanlong1004@gmail.com>
2022-01-04 19:21:05 -0500

    Updated test scripts

--- scripts/q.sh
+++ scripts/q.sh
@@ -1,1 +1,1 @@
-esmf-profiler -t ./tests/fixtures/test-traces -n "test20211122" -o "test20211122" -p 'https://github.com/ryanlong1004/automatic-succotash'
+esmf-profiler -v -t ./tests/fixtures/test-traces-large/traceout -n "test-_-20211122" -o "test20211122" -p 'https://github.com/ryanlong1004/automatic-succotash'
