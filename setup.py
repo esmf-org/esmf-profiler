9f62a70860f175322c30a6a8902b714aeffee3a0
Ryan Long <ryanlong1004@gmail.com>
2022-01-06 20:02:28 -0500

    Added bump2version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.2.0",  # Required
+    version="0.2.1",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -163,6 +163,7 @@ setup(
             "pytest",
             "pytest-cov",
             "pytest-xdist",
+            "bump2version",
         ],
         "test": ["pytest", "pytest-cov", "pytest-forked", "pytest-xdist"],
     },
